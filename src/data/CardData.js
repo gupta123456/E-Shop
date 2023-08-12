2213483fd0c4ba4ee7dfab3f9193676046dc2da5
yashK98 <yash.4198@gmail.com>
2023-08-12 14:03:49 +0530

    [Yashwant][Initial commit]

---
+++ src/data/CardData.js
@@ -0,0 +1,62 @@
+export const cardData = [
+    {
+        "id" : 1,
+        "heading" : "First Card",
+        "body" : "Sample Body",
+        "footer" : "Sample Footer"
+    },
+    {
+        "id" : 2,
+        "heading" : "First Card",
+        "body" : "Sample Body",
+        "footer" : "Sample Footer"
+    },
+    {
+        "id" : 3,
+        "heading" : "First Card",
+        "body" : "Sample Body",
+        "footer" : "Sample Footer"
+    },
+    {
+        "id" : 4,
+        "heading" : "First Card",
+        "body" : "Sample Body",
+        "footer" : "Sample Footer"
+    },
+    {
+        "id" : 5,
+        "heading" : "First Card",
+        "body" : "Sample Body",
+        "footer" : "Sample Footer"
+    },
+    {
+        "id" : 6,
+        "heading" : "First Card",
+        "body" : "Sample Body",
+        "footer" : "Sample Footer"
+    },
+    {
+        "id" : 7,
+        "heading" : "First Card",
+        "body" : "Sample Body",
+        "footer" : "Sample Footer"
+    },
+    {
+        "id" : 8,
+        "heading" : "First Card",
+        "body" : "Sample Body",
+        "footer" : "Sample Footer"
+    },
+    {
+        "id" : 9,
+        "heading" : "First Card",
+        "body" : "Sample Body",
+        "footer" : "Sample Footer"
+    },
+    {
+        "id" : 10,
+        "heading" : "First Card",
+        "body" : "Sample Body",
+        "footer" : "Sample Footer"
+    }
+]
